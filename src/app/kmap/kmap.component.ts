2f8eb3d909460806dbd92345e7b8eff3a271c06e
Arcadius19 <arek.mikolajczak@mail.utoronto.ca>
2018-02-01 18:25:54 +0000

    Renamed variables and method names. Started working on finding the best groups (initiated an array that keeps track of cells that are already included in some group)

--- src/app/kmap/kmap.component.ts
+++ src/app/kmap/kmap.component.ts
@@ -12,13 +12,13 @@ export class KmapComponent implements OnInit {
   FLAG_C = 2; // 0010
   FLAG_D = 1; // 0001
 
-  n_rows = 4;
-  n_columns = 4;
+  nRows = 4;
+  nColumns = 4;
 
-  kmap_evals_matrix: number[][] = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
-  kmap_binary: number[][] = [[], [], [], []];
+  kmapEvaluations: number[][] = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+  kmapIDs: number[][] = [[], [], [], []];
 
-  evaluate_expr(): Boolean {
+  evaluateExpression(): Boolean {
     let a_var: Boolean;
     let b_var: Boolean;
     let c_var: Boolean;
@@ -30,15 +30,15 @@ export class KmapComponent implements OnInit {
     let expr;
     try {
       expr = parser.parse(query);
-      for (let i in this.kmap_binary) {
-        for (let j in this.kmap_binary[i]) {
-          let dec_number = this.kmap_binary[i][j];
+      for (let i in this.kmapIDs) {
+        for (let j in this.kmapIDs[i]) {
+          let dec_number = this.kmapIDs[i][j];
           a_var = !!(dec_number & this.FLAG_A);
           b_var = !!(dec_number & this.FLAG_B);
           c_var = !!(dec_number & this.FLAG_C);
           d_var = !!(dec_number & this.FLAG_D);
           evaluation = expr.evaluate({ A: a_var, B: b_var, C: c_var, D: d_var });
-          this.kmap_evals_matrix[i][j] = (evaluation) ? 1 : 0;
+          this.kmapEvaluations[i][j] = (evaluation) ? 1 : 0;
         }
       }
     } catch (err) {
@@ -51,12 +51,12 @@ export class KmapComponent implements OnInit {
   constructor(private parserService: ParserService) { }
 
   ngOnInit() {
-    this.evaluate_expr();
+    this.evaluateExpression();
 
     const binaries = ['00', '01', '11', '10'];
     for (let i in binaries) {
       for (let j in binaries) {
-        this.kmap_binary[i][j] = parseInt(binaries[i] + binaries[j], 2);
+        this.kmapIDs[i][j] = parseInt(binaries[i] + binaries[j], 2);
       }
     }
 
@@ -64,17 +64,46 @@ export class KmapComponent implements OnInit {
 
   // check if a group of cells starting at [off_row][off_col] spanning to [off_row+ran_row][off_col+ran_col] is a valid group, i.e.
   // all cells in a group evaluate the expression to true
-  check_group(off_row: number, off_col: number, ran_row: number, ran_col, number): boolean {
-    let is_valid = true;
-    for (let i = off_row; i < off_row + ran_row; i++) {
-      for (let j = off_col; j < off_col + ran_col; j++) {
-        if (this.kmap_evals_matrix[i % this.n_rows][j % this.n_columns] == 0) {
-          is_valid = false;
+  checkGroup(offRow: number, offCol: number, rangeRow: number, rangeCol: number): boolean {
+    let isValid = true;
+    for (let i = offRow; i < offRow + rangeRow; i++) {
+      for (let j = offCol; j < offCol + rangeCol; j++) {
+        if (this.kmapEvaluations[i % this.nRows][j % this.nColumns] == 0) {
+          isValid = false;
           break;
         }
       }
     }
-    return is_valid;
+    return isValid;
+  }
+
+  markGroup(group: number[][], offRow: number, offCol: number, rangeRow: number, rangeCol: number): number[][] {
+    for (let i = offRow; i < offRow + rangeRow; i++) {
+      for (let j = offCol; j < offCol + rangeCol; j++) {
+        group[i % this.nRows][j % this.nColumns] = 0;
+      }
+    }
+    return group;
+  }
+
+  findBestGroups(): number[][] {
+    let markedKMap = this.kmapEvaluations.slice();
+    let bestGroups: number[][];
+
+    // Check 16x
+    if (this.checkGroup(0, 0, 4, 4)) {
+      bestGroups.push([0, 0, 4, 4]);
+    }
+
+    // Check 8x
+    for (let i = 0 ; i < this.nRows; i++) {
+      this.checkGroup(i, 0, 2, 4);
+    }
+    for (let j; j < this.nColumns; j++) {
+      this.checkGroup(0, j, 4, 2);
+    }
+
+    return bestGroups;
   }
 
 }

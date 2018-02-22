5b34c4d66bf785042e563761428a1ef4dd8d2954
Arcadius19 <arek.mikolajczak@mail.utoronto.ca>
2018-02-22 11:51:27 +0000

    Added feedback comments to Exr-to-kmap exercises depending on the user's answer

--- src/app/exercises/ex-expr-to-kmap/ex-expr-to-kmap.component.ts
+++ src/app/exercises/ex-expr-to-kmap/ex-expr-to-kmap.component.ts
@@ -18,12 +18,16 @@ export class ExExprToKmapComponent implements OnInit {
   private interKmapComponent: InteractiveKmapComponent;
 
   exercise$: Observable<ExExprToKmap>;
-  markTrue: boolean;
-  nVars = 4;  // may be changed to dynamic in future
-  latexExpression: string;
-  solution: number[][];
-  correct: boolean;
-  resultStyle: string;
+  markTrue: boolean;          // whether user should select true or false states
+  nVars = 4;                  // may be changed to dynamic in future
+  latexExpression: string;    // expression in LaTeX form
+  solution: number[][];       // KMap grid with correct answer
+  correct: boolean;           // whether user's answer was correct or not
+  resultStyle: string;        // style for panels (panel-success or panel-danger) depending on 'correct' boolean
+  nGuessed: number;           // number of states user marked correctly
+  nTrue: number;              // number of states user should mark (all true/false states)
+  nTrueCorrect: number;       // number of true/false states user marked
+  nTrueIncorrect: number;     // number of non-true/false states user marked but should not
 
   constructor(
     private route: ActivatedRoute,
@@ -51,15 +55,20 @@ export class ExExprToKmapComponent implements OnInit {
       this.solution = (new KarnaughMap(this.nVars)).evaluate(exercise.expression);
       this.correct = null;
       this.resultStyle = null;
+      this.nGuessed = 0;
+      this.nTrue = 0;
+      this.nTrueCorrect = 0;
+      this.nTrueIncorrect = 0;
     });
   }
 
   onVerify() {
+    this.nGuessed = 0;
+    this.nTrue = 0;
+    this.nTrueCorrect = 0;
+    this.nTrueIncorrect = 0;
+
     let result = true;
-    let nGuessed = 0;
-    let nTrue = 0;
-    let nTrueCorrect = 0;
-    let nTrueIncorrect = 0;
     let solutionMarked: number[][];
 
     if (this.markTrue) {
@@ -71,13 +80,13 @@ export class ExExprToKmapComponent implements OnInit {
 
     for (let i in this.interKmapComponent.marked) {
       for (let j in solutionMarked) {
-        if (solutionMarked[i][j] == 1) { nTrue++; }
+        if (solutionMarked[i][j] == 1) { this.nTrue++; }
         if (this.interKmapComponent.marked[i][j] == solutionMarked[i][j]) {
-          nGuessed++;
-          if (solutionMarked[i][j] == 1) { nTrueCorrect++; }
+          this.nGuessed++;
+          if (solutionMarked[i][j] == 1) { this.nTrueCorrect++; }
         } else {
           result = false;
-          if (solutionMarked[i][j] == 0) { nTrueIncorrect++; }
+          if (solutionMarked[i][j] == 0) { this.nTrueIncorrect++; }
         }
       }
     }
@@ -91,9 +100,9 @@ export class ExExprToKmapComponent implements OnInit {
     }
 
     console.log('=== ', result, ' ===');
-    console.log('You have guessed: ', nGuessed, ' in total');
-    console.log('You have marked ', nTrueCorrect, ' out of ', nTrue);
-    console.log('You should have not marked ', nTrueIncorrect, ' squares');
+    console.log('You have guessed: ', this.nGuessed, ' in total');
+    console.log('You have marked ', this.nTrueCorrect, ' out of ', this.nTrue);
+    console.log('You should have not marked ', this.nTrueIncorrect, ' squares');
   }
 
 }

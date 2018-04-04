aa7665b32bf6fd0917ff900e8f51add950a63618
Arcadius19 <arek.mikolajczak@mail.utoronto.ca>
2018-04-05 00:27:16 +0100

    Added a database connection (may not be functional)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const { Client } = require('pg');
 
 const app = express();
 
@@ -10,8 +11,27 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log('Running'));
+
+app.post("/api/feedback", function(req, res) {
+
+});

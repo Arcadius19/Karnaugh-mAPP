0dbd167f8d24866d749c9153de64c9bd76582792
Arcadius19 <arek.mikolajczak@mail.utoronto.ca>
2018-04-05 20:59:02 +0100

    Fixed success alert in Feedback tab. Improved a set of Expr-to-kmap questions

--- src/app/exercises/exercise-types/expr-to-kmap/ex-expr-to-kmap.service.ts
+++ src/app/exercises/exercise-types/expr-to-kmap/ex-expr-to-kmap.service.ts
@@ -6,10 +6,22 @@ import {Exercise} from '../../exercise';
 import {MathJaxConverter} from '../../../auxiliary/mathjax-aux/math-jax-converter';
 
 export class ExExprToKmap extends Exercise {
+  static currentPracticeID = 1;
+  static currentQuizID = 1;
+
   expression: string;
   name: string;
 
-  constructor(id: number, expression: string, points: number) {
+  constructor(expression: string, points: number, isQuiz = false) {
+    let id: number;
+    if (isQuiz) {
+      id = ExExprToKmap.currentQuizID;
+      ExExprToKmap.currentQuizID++;
+    } else {
+      id = ExExprToKmap.currentPracticeID;
+      ExExprToKmap.currentPracticeID++;
+    }
+
     super(id, points);
     this.expression = expression;
     this.name = id + '. ' + MathJaxConverter.toBrowserText(expression);
@@ -17,21 +29,39 @@ export class ExExprToKmap extends Exercise {
 }
 
 let EXERCISES_TEST = [
-  new ExExprToKmap(1, 'A', 1),
-  new ExExprToKmap(2, 'A or B', 2),
-  new ExExprToKmap(3, 'A and C', 2),
-  new ExExprToKmap(4, 'A <=> B', 2),
-  new ExExprToKmap(5, 'B => D', 2),
-  new ExExprToKmap(6, 'A or C <=> D', 3),
+  new ExExprToKmap('D', 1, true),
+  new ExExprToKmap('A or B', 2, true),
+  new ExExprToKmap('B or D', 2, true),
+  new ExExprToKmap('C or not C', 2, true),
+  new ExExprToKmap('A and C', 2, true),
+  new ExExprToKmap('A <=> B', 2, true),
+  new ExExprToKmap('B => D', 2, true),
+  new ExExprToKmap('D ? A : B', 3, true),
+  new ExExprToKmap('A or C <=> D', 3, true),
+  new ExExprToKmap('(A or not C) and (not D or C)', 3, true),
+  new ExExprToKmap('D and C <=> B => A', 4, true),
+  new ExExprToKmap('A <=> not B <=> C', 4, true),
+  new ExExprToKmap('D <=> B <=> A <=> C', 4, true),
+  new ExExprToKmap('not A <=> C => B <=> D', 5, true),
+  new ExExprToKmap('A ? (B  <=> C) : not D', 5, true)
 ];
 
 let EXERCISES_PRACTICE = [
-  new ExExprToKmap(1, 'A', 0),
-  new ExExprToKmap(2, 'C or B', 0),
-  new ExExprToKmap(3, 'A and D', 0),
-  new ExExprToKmap(4, 'A <=> B', 0),
-  new ExExprToKmap(5, 'D => C', 0),
-  new ExExprToKmap(6, 'B or A <=> D', 0),
+  new ExExprToKmap('A', null),
+  new ExExprToKmap('C or B', null),
+  new ExExprToKmap('A or D', null),
+  new ExExprToKmap('B and not B', null),
+  new ExExprToKmap('A and D', null),
+  new ExExprToKmap('A <=> B', null),
+  new ExExprToKmap('D => C', null),
+  new ExExprToKmap('A ? B : C', null),
+  new ExExprToKmap('B or A <=> C', null),
+  new ExExprToKmap('(A or not D) and (B or D)', null),
+  new ExExprToKmap('B and C <=> A => C', null),
+  new ExExprToKmap('C <=> B <=> D', null),
+  new ExExprToKmap('D <=> C <=> B <=> A', null),
+  new ExExprToKmap('A <=> B => D <=> C', null),
+  new ExExprToKmap('A ? (B  => C) : D', null)
 ];
 
 // @Injectable()
